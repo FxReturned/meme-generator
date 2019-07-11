6dc43c725b1bdb471d58fc54545868ce4825d618
FxReturned <46070551+FxReturned@users.noreply.github.com>
2019-07-10 20:38:07 -0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header() {
             <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
                  alt="Problems?"
             />
-            <p>Meme GeneratoRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRr</p>
+            <p>Meme Generator</p>
       </header>
     )
 }
