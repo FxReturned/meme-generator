9832ba15a444dd55befde2d49298e38c6d7968ca
Cesar Cordova <cesar.cor.itu@gmail.com>
2019-07-10 20:32:02 -0500

    RRRRR

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header() {
             <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
                  alt="Problems?"
             />
-            <p>Meme Generator</p>
+            <p>Meme GeneratoRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRr</p>
       </header>
     )
 }
